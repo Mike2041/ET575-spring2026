a78f147029c56b418c089671271c2c9aed61d010
Mike2041 <caduarmi@gmail.com>
2026-03-23 15:48:16 +0000

    Lab15 Ranwom Numbers

---
+++ Lab15_Cabrera/lab15_function.Cabrera.cpp
@@ -0,0 +1,48 @@
+/*
+Miguel Eduardo Cabrera Callejas
+Lab15, Random Numbers
+March 03/23/2026
+*/
+
+#include<iostream>
+#include<cstdlib>
+
+#include<time.h>
+
+// Example 1: rand() and srand() functions
+using namespace std;
+int randomnumber(){
+    srand(time(0));
+    return rand();
+}
+
+// Example 2: range of random nubmers
+// Generate numbers between 0 and 9 (inclusive)
+int random_0_9(){
+    srand(time(0));
+    return rand()%10;
+}
+
+// Example 3: generate a number between 5 and 12
+int random_5_12(){
+    srand(time(0));
+    return 5 + rand()%8;
+
+}
+
+// Example 4: match a number of a pair of dices 
+// Function randomly generates number between 1 and 6
+int rolldice(){
+    srand(time(0));
+    return 1+rand()%6;
+}
+// Function to check if two numbers are the same
+bool matchdices(int roll1, int roll2){
+    return roll1 == roll2;
+}
+
+// Function to print result
+void printdices(int roll1, int roll2, bool result){
+    cout<<"Is "<<roll1<<" the same as "<<roll2<<" ? "<<result<<endl;
+    
+}
---
+++ Lab15_Cabrera/lab15_main_Cabrera.cpp
@@ -0,0 +1,38 @@
+/*
+Miguel Eduardo Cabrera Callejas
+Lab15, Random Numbers
+March 03/23/2026
+*/
+
+#include<iostream>
+
+using namespace std;
+
+#include "lab15_function.Cabrera.cpp"
+
+int main (){
+    srand(time(nullptr));
+
+    cout<<"\n ---- Example 1: Random number ----- "<<endl;
+    cout<<randomnumber()<<endl;
+    
+    cout<<"\n ---- Example 2: random number between 0 and 9 ----- "<<endl;
+    cout<<random_0_9()<<endl;
+    
+    cout<<"\n ---- Example 3: random number between 5 and 12 ----- "<<endl;
+    cout<<random_5_12()<<endl; 
+    
+
+
+    cout<<"\n ---- Example 4: matching number ----- "<<endl;
+    int roll1 = 1+rand()%6;
+    cout<<endl<<endl;
+    int roll2 = 1+rand()%6;
+    bool resultmatch = matchdices(roll1,roll2);
+    printdices(roll1,roll2,resultmatch);
+
+    return 0;
+}
+
+
+
